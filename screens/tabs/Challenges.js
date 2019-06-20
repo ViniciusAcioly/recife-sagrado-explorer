204bc04f2a1a367ed0312c0dc92a83d419217c66
vinis <vinicius.acioly@hotmail.com>
2019-06-20 10:26:24 -0300

    costumização básica das tabs

--- screens/tabs/Challenges.js
+++ screens/tabs/Challenges.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, StatusBar, Text, Button, View, Platform, Alert } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, ImageBackground, Button, StatusBar, Alert, SafeAreaView,
+	TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, TouchableOpacity, } from 'react-native';
 
 import ApiKeys from '../../constants/ApiKeys'; //dados de autenticação do firebase
 import * as firebase from 'firebase';
@@ -17,16 +18,38 @@ class Challenges extends React.Component {
     //this.props.watchUserData();
   }
 
-  //Ao pressionar sair
-  onSignoutPress = () => {
-    firebase.auth().signOut();
+  //Ver desafios
+  onChallengePress = () => {
+
+  }
+
+  //Ver mapa
+  onMapPress = () => {
+    
   }
 
   render() {
     return (
-      <View style={{flex: 1, flexDirection: "column", paddingVertical: 50, paddingHorizontal: 10,}}>
-        <Button title="Sair" onPress={this.onSignoutPress} />
-      </View>
+    	<SafeAreaView style = {styles.container}>
+			<StatusBar barStyle="light-content" />
+				<TouchableWithoutFeedback style={styles.container} onPress={Keyboard.dismiss}>
+					<View style = {styles.container}>
+						<View style = {styles.logoContainer}>
+							<Image style = {styles.logo}
+								source={require('../../assets/images/logo.png')}>
+							</Image>
+						</View>
+
+						<TouchableOpacity style={styles.buttonContainer} onPress={this.onChallengePress}>
+							<Text style={styles.buttonText}>MEUS DESAFIOS</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity style={styles.buttonContainer} onPress={this.onMapPress}>
+							<Text style={styles.buttonText}>VER MAPA</Text>
+						</TouchableOpacity>
+					</View>
+				</TouchableWithoutFeedback>				
+		</SafeAreaView>
 
     );
   }
@@ -34,8 +57,59 @@ class Challenges extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  text: { color: "white", fontWeight: "bold", textAlign: "center", fontSize: 20, },
-  textInput: { borderWidth:1, borderColor:"gray", marginVertical: 20, padding:10, height:40, alignSelf: "stretch", fontSize: 18, },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		backgroundColor: 'rgb(32, 53, 70)',
+		flexDirection: 'column',
+	},
+	logoContainer: {
+		alignItems: 'center',
+		marginVertical: 10,
+	},
+	logo: {
+		width: 80,
+		height: 80,
+	},
+	text: {
+		fontWeight: 'bold',
+		fontSize: 18,
+		alignItems: 'center',
+		textAlign: 'center',
+		color: '#fff',
+		bottom: 10,
+	},
+	input: {
+		height: 40,
+		alignItems: 'center',
+		backgroundColor: 'rgba(255, 255, 255, 0.2)',
+		color: '#fff',
+		marginHorizontal: 45,
+		marginVertical: 10,
+		paddingHorizontal: 10,
+		borderRadius: 10,
+	},
+	inputGroup: {
+		bottom: 50,
+	},
+	buttonContainer: {
+		height: 40,
+		backgroundColor: '#f7c744',
+		marginHorizontal: 45,
+		marginVertical: 10,
+		borderRadius: 10,
+	},
+	buttonReg: {
+		height: 45,
+		backgroundColor: 'rgba(255, 255, 255, 0.1)',
+	},
+	buttonText: {
+		fontWeight: 'bold',
+		textAlign: 'center',
+		alignItems: 'center',
+		marginVertical: 10,
+	},
+
 });
 
 export default Challenges;
